889229819b0ece9561520c9e7079d43a6ebe877d
House-lovers7 <taisuke.got@gmail.com>
2021-10-14 14:25:30 +0900

    Controllerで設定した変数の値をviewで表示

--- resources/views/hello/index.php
+++ resources/views/hello/index.php
@@ -3,12 +3,7 @@
 <head>
     <title>Hello/Index</title>
     <style>
-        body {
-            font-size: 16pt;
-            color: #999;
-        }
-
-        h1 {
+        <body><h1>Index</h1><p><?php echo $msg; ?></p><p><?php echo date("Y年n月j日"); ?></p></body>h1 {
             font-size: 100pt;
             text-align: right;
             color: #f6f6f6;
